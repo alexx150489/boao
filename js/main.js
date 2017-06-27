71af85888069d55982bd11b68b08c73aae30484a
alexx150489 <nikolskiy150489@gmail.com>
2017-06-27 20:57:38 +0400

    pravki

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-	// ancors
-	$(".block-sidebar .link1").on("click", function(e) {
+	// ancor
+	$(".block-sidebar .link").on("click", function(e) {
 		e.preventDefault();
 		var currentBlock = $(this).attr("href"),
 		currentBlockOffset = $(currentBlock).offset().top;
@@ -9,84 +9,11 @@ $(document).ready(function() {
 			scrollTop: currentBlockOffset
 		}, 500);
 	});
-
-	$(".block-sidebar .link2").on("click", function(e) {
-		e.preventDefault();
-		var currentBlock = $(this).attr("href"),
-		currentBlockOffset = $(currentBlock).offset().top;
-		$("html, body").animate({
-			scrollTop: currentBlockOffset
-		}, 500);
-	});
-
-	$(".block-sidebar .link3").on("click", function(e) {
-		e.preventDefault();
-		var currentBlock = $(this).attr("href"),
-		currentBlockOffset = $(currentBlock).offset().top;
-		$("html, body").animate({
-			scrollTop: currentBlockOffset
-		}, 500);
-	});
-
-	$(".block-sidebar .link4").on("click", function(e) {
-		e.preventDefault();
-		var currentBlock = $(this).attr("href"),
-		currentBlockOffset = $(currentBlock).offset().top;
-		$("html, body").animate({
-			scrollTop: currentBlockOffset
-		}, 500);
-	});
-
-	$(".block-sidebar .link5").on("click", function(e) {
-		e.preventDefault();
-		var currentBlock = $(this).attr("href"),
-		currentBlockOffset = $(currentBlock).offset().top;
-		$("html, body").animate({
-			scrollTop: currentBlockOffset
-		}, 500);
-	});
-
-	$(".block-sidebar .link6").on("click", function(e) {
-		e.preventDefault();
-		var currentBlock = $(this).attr("href"),
-		currentBlockOffset = $(currentBlock).offset().top;
-		$("html, body").animate({
-			scrollTop: currentBlockOffset
-		}, 500);
-	});
-
-	$(".block-sidebar .link7").on("click", function(e) {
-		e.preventDefault();
-		var currentBlock = $(this).attr("href"),
-		currentBlockOffset = $(currentBlock).offset().top;
-		$("html, body").animate({
-			scrollTop: currentBlockOffset
-		}, 500);
-	});
-
-	$(".block-sidebar .link8").on("click", function(e) {
-		e.preventDefault();
-		var currentBlock = $(this).attr("href"),
-		currentBlockOffset = $(currentBlock).offset().top;
-		$("html, body").animate({
-			scrollTop: currentBlockOffset
-		}, 500);
-	});
-
-	$(".block-sidebar .link9").on("click", function(e) {
-		e.preventDefault();
-		var currentBlock = $(this).attr("href"),
-		currentBlockOffset = $(currentBlock).offset().top;
-		$("html, body").animate({
-			scrollTop: currentBlockOffset
-		}, 500);
-	});
-
 
 
 	// slcik slider http://kenwheeler.github.io/slick/ >>>
-	// block-meditation
-	$('.meditation-gallery .slider').slick({
+
+	$('.slider.slick1').slick({
 		autoplay: true,
 		pauseOnHover: true,
 		autoplaySpeed: 3000,
@@ -94,119 +21,11 @@ $(document).ready(function() {
 		arrows: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		prevArrow: $('.meditation-gallery .slider-prev'),
-		nextArrow: $('.meditation-gallery .slider-next')
-	});
-	// block-sport
-		$('.sport-gallery .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.sport-gallery .slider-prev'),
-		nextArrow: $('.sport-gallery .slider-next')
-	});
-		// block-sea
-		$('.sea-gallery1 .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.sea-gallery1 .slider-prev'),
-		nextArrow: $('.sea-gallery1 .slider-next')
-	});
-	// block-sea
-	$('.sea-gallery2 .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.sea-gallery2 .slider-prev'),
-		nextArrow: $('.sea-gallery2 .slider-next')
-	});
-	// block-seafood
-	$('.seafood-gallery .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.seafood-gallery .slider-prev'),
-		nextArrow: $('.seafood-gallery .slider-next')
-	});
-	// block-seafood
-		$('.seafood-gallery2 .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.seafood-gallery2 .slider-prev'),
-		nextArrow: $('.seafood-gallery2 .slider-next')
-	});
-	// block-international
-		$('.international-gallery1 .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.international-gallery1 .slider-prev'),
-		nextArrow: $('.international-gallery1 .slider-next')
-	});
-	// block-music
-		$('.music-gallery1 .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.music-gallery1 .slider-prev'),
-		nextArrow: $('.music-gallery1 .slider-next')
-	});
-	// block-music
-		$('.music-gallery2 .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.music-gallery2 .slider-prev'),
-		nextArrow: $('.music-gallery2 .slider-next')
-	});
-		// block-honeymoon
-		$('.honeymoon-gallery1 .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.honeymoon-gallery1 .slider-prev'),
-		nextArrow: $('.honeymoon-gallery1 .slider-next')
+		prevArrow: $('.slick1-prev'),
+		nextArrow: $('.slick1-next')
 	});
 
-	$('.honeymoon-gallery2 .slider').slick({
+		$('.slider.slick2').slick({
 		autoplay: true,
 		pauseOnHover: true,
 		autoplaySpeed: 3000,
@@ -214,11 +33,11 @@ $(document).ready(function() {
 		arrows: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		prevArrow: $('.honeymoon-gallery2 .slider-prev'),
-		nextArrow: $('.honeymoon-gallery2 .slider-next')
+		prevArrow: $('.slick2-prev'),
+		nextArrow: $('.slick2-next')
 	});
 
-		$('.honeymoon-gallery3 .slider').slick({
+		$('.slider.slick3').slick({
 		autoplay: true,
 		pauseOnHover: true,
 		autoplaySpeed: 3000,
@@ -226,56 +45,8 @@ $(document).ready(function() {
 		arrows: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		prevArrow: $('.honeymoon-gallery3 .slider-prev'),
-		nextArrow: $('.honeymoon-gallery3 .slider-next')
+		prevArrow: $('.slick3-prev'),
+		nextArrow: $('.slick3-next')
 	});
 
-		// nation
-		$('.block-nation .nation-gallery1 .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.block-nation .nation-gallery1 .slider-prev'),
-		nextArrow: $('.block-nation .nation-gallery1 .slider-next')
-	});
-
-		$('.block-nation .nation-gallery2 .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.block-nation .nation-gallery2 .slider-prev'),
-		nextArrow: $('.block-nation .nation-gallery2 .slider-next')
-	});
-
-		$('.block-nation .nation-gallery3 .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.block-nation .nation-gallery3 .slider-prev'),
-		nextArrow: $('.block-nation .nation-gallery3 .slider-next')
-	});
-		// medtravel
-		$('.block-medtravel .medtravel-gallery1 .slider').slick({
-		autoplay: true,
-		pauseOnHover: true,
-		autoplaySpeed: 3000,
-		dots: false,
-		arrows: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: $('.block-medtravel .medtravel-gallery1 .slider-prev'),
-		nextArrow: $('.block-medtravel .medtravel-gallery1 .slider-next')
-	});
 });
